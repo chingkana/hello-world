148ece5d3354273e8583ac3f88501c1c8545b8c8
ching <braveitdude@gmail.com>
2018-01-28 12:45:55 +0530

    static server setup

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,1 +1,7 @@
-//console.log("start");
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+    <h1>Hello World !!!</h1>,
+    document.getElementById("content")
+)
